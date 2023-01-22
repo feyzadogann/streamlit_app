ca17a6f8de58077ff5258db0ff4fba10d9c37101
Feyza Dogan <36421705+feyzadogann@users.noreply.github.com>
2023-01-22 22:03:17 +0300

    Update streamlit_app.py
    subheader changed & text added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit as st
 st.title('My Parents New Healthy Diner')
-st.subheader('Breakfast')
+st.subheader('Breakfast Menu')
+st.text('Omega 3 & Blueberry Oatmeal')
+st.text('Kale, Spinach & Rocket Smoothie')
+st.text('Hard-Boiled Free-Range Egg')
