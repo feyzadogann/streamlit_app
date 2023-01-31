b359bcceed122d3a66d57eec7c1ab01222f97bb3
Feyza Dogan <36421705+feyzadogann@users.noreply.github.com>
2023-01-31 16:06:22 +0300

    Update streamlit_app.py
    fruit choice taken from user

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,9 @@ st.dataframe(fruits_to_show)
 
 # New section to display fruityvice api response
 st.header('Fruityvice Fruit Advice!')
-fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + "kiwi")
+fruit_choice = st.text_input("What fruit would you like information about?", 'Kiwi')
+st.write('The user entered ,' fruit_choice)
+fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
 st.dataframe(fruityvice_normalized)
