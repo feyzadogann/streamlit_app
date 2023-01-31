0ebd84e8b0e96cdcf5c9fc0c2171f1ee4f354010
Feyza Dogan <36421705+feyzadogann@users.noreply.github.com>
2023-01-31 16:07:03 +0300

    Update streamlit_app.py
    typo fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ st.dataframe(fruits_to_show)
 # New section to display fruityvice api response
 st.header('Fruityvice Fruit Advice!')
 fruit_choice = st.text_input("What fruit would you like information about?", 'Kiwi')
-st.write('The user entered ,' fruit_choice)
+st.write('The user entered ', fruit_choice)
 fruityvice_response = rq.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
