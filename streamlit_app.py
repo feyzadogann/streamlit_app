d9e33d9a974fe07ca8c0950d3e4ae19d32e8b7d2
Feyza Dogan <36421705+feyzadogann@users.noreply.github.com>
2023-02-07 17:27:50 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,8 @@ st.dataframe(fruits_to_show)
 # New section to display fruityvice api response
 st.header('Fruityvice Fruit Advice!')
 try:
-  fruit_choice = st.text_input("What fruit would you like information about?", 'Kiwi')
+  fruit_choice = st.text_input("What fruit would you like information about?")
+  print(fruit_choice)
   if not fruit_choice:
     st.error("Please select a fruit to get information")
   else:
